JohnZengshi/ferdium-app

commit d9990eb173c6c85c9ca2c783f32a9eae850a8cbd

Changed occurrences of 'Franz' to 'Ferdi' (probably missed earlier while catchup from Franz source). [skip ci]

src/api/utils/auth.js
@@ -8,11 +8,11 @@ export const prepareAuthRequest = (options = { method: 'GET' }, auth = true) => 
     mode: 'cors',
     headers: Object.assign({
       'Content-Type': 'application/json',
-      'X-Franz-Source': 'desktop',
-      'X-Franz-Version': app.getVersion(),
-      'X-Franz-platform': process.platform,
-      'X-Franz-Timezone-Offset': new Date().getTimezoneOffset(),
-      'X-Franz-System-Locale': app.getLocale(),
+      'X-Ferdi-Source': 'desktop',
+      'X-Ferdi-Version': app.getVersion(),
+      'X-Ferdi-platform': process.platform,
+      'X-Ferdi-Timezone-Offset': new Date().getTimezoneOffset(),
+      'X-Ferdi-System-Locale': app.getLocale(),
     }, options.headers),
   });
 

src/components/services/content/ConnectionLostBanner.js
@@ -14,7 +14,7 @@ import { LIVE_API_WEBSITE } from '../../../config';
 const messages = defineMessages({
   text: {
     id: 'connectionLostBanner.message',
-    defaultMessage: '!!!Oh no! Franz lost the connection to {name}.',
+    defaultMessage: '!!!Oh no! Ferdi lost the connection to {name}.',
   },
   moreInformation: {
     id: 'connectionLostBanner.informationLink',

src/components/services/tabs/TabItem.js
@@ -12,7 +12,7 @@ import { observable, autorun } from 'mobx';
 import ServiceModel from '../../../models/Service';
 import { ctrlKey, cmdKey } from '../../../environment';
 
-const IS_SERVICE_DEBUGGING_ENABLED = (localStorage.getItem('debug') || '').includes('Franz:Service');
+const IS_SERVICE_DEBUGGING_ENABLED = (localStorage.getItem('debug') || '').includes('Ferdi:Service');
 
 const { Menu, dialog, app } = remote;
 

src/config.js
@@ -212,7 +212,7 @@ export const ALLOWED_PROTOCOLS = [
   'https:',
   'http:',
   'ftp:',
-  'franz:',
+  'ferdi:',
 ];
 
 export const PLANS = {

src/electron/ipc-api/cld.js
@@ -1,7 +1,7 @@
 import { ipcMain } from 'electron';
 import cld from 'cld';
 
-const debug = require('debug')('Franz:ipcApi:cld');
+const debug = require('debug')('Ferdi:ipcApi:cld');
 
 export default async () => {
   ipcMain.handle('detect-language', async (event, { sample }) => {

src/electron/ipc-api/dnd.js
@@ -1,7 +1,7 @@
 import { ipcMain } from 'electron';
 import { getDoNotDisturb } from '@meetfranz/electron-notification-state';
 
-const debug = require('debug')('Franz:ipcApi:dnd');
+const debug = require('debug')('Ferdi:ipcApi:dnd');
 
 export default async () => {
   ipcMain.handle('get-dnd', async () => {

src/electron/macOSPermissions.js
@@ -10,7 +10,7 @@ if (isMac) {
   askForScreenCaptureAccess = require('node-mac-permissions').askForScreenCaptureAccess;
 }
 
-const debug = require('debug')('Franz:macOSPermissions');
+const debug = require('debug')('Ferdi:macOSPermissions');
 
 const permissionExists = macosVersion.isGreaterThanOrEqualTo('10.15');
 const filePath = path.join(app.getPath('userData'), '.has-app-requested-screen-capture-permissions');
@@ -49,7 +49,6 @@ function createStatusFile() {
   }
 }
 
-
 export default async function (mainWindow) {
   debug('Checking camera & microphone permissions');
   systemPreferences.askForMediaAccess('camera');
@@ -61,7 +60,7 @@ export default async function (mainWindow) {
     const { response } = await dialog.showMessageBox(mainWindow, {
       type: 'info',
       message: 'Enable Screen Sharing',
-      detail: 'To enable screen sharing for some services, Franz needs the permission to record your screen.',
+      detail: 'To enable screen sharing for some services, Ferdi needs the permission to record your screen.',
       buttons: [
         'Allow screen sharing',
         'No',

src/features/planSelection/api.js
@@ -2,7 +2,7 @@ import { sendAuthRequest } from '../../api/utils/auth';
 import { API, API_VERSION } from '../../environment';
 import Request from '../../stores/lib/Request';
 
-const debug = require('debug')('Franz:feature:planSelection:api');
+const debug = require('debug')('Ferdi:feature:planSelection:api');
 
 export const planSelectionApi = {
   downgrade: async () => {

commit 79ec0ef3d19e18a1872905e8c95dcafd04f4ff3f

Use X-Franz headers to talk to the server (#1429)
d9990eb broke communication with the internal server by using X-Ferdi
headers instead of X-Franz headers on HTTP requests.
For compatibility reasons, we retain the X-Franz headers insteaf of
X-Ferdi headers in the internal server.

This commits reverts the change to the headers to restore internal
server functionality.

src/api/utils/auth.js
@@ -8,11 +8,11 @@ export const prepareAuthRequest = (options = { method: 'GET' }, auth = true) => 
     mode: 'cors',
     headers: Object.assign({
       'Content-Type': 'application/json',
-      'X-Ferdi-Source': 'desktop',
-      'X-Ferdi-Version': app.getVersion(),
-      'X-Ferdi-platform': process.platform,
-      'X-Ferdi-Timezone-Offset': new Date().getTimezoneOffset(),
-      'X-Ferdi-System-Locale': app.getLocale(),
+      'X-Franz-Source': 'desktop',
+      'X-Franz-Version': app.getVersion(),
+      'X-Franz-platform': process.platform,
+      'X-Franz-Timezone-Offset': new Date().getTimezoneOffset(),
+      'X-Franz-System-Locale': app.getLocale(),
     }, options.headers),
   });
 
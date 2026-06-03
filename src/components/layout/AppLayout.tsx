29c054752689fccef13f44a60ee809890e555e31
John <johnzengshi@gmail.com>
2026-06-03 16:35:48 +0800

    fix: 保持服务 webview 永久挂载以解决模块切换时 polling 报错
    切换模块时不再条件卸载服务 webview，改用 display:none 隐藏。确保 webview 始终在 DOM 中，polling timer 不会打到已离树的 webview。
    
    Ultraworked with [Sisyphus](https://github.com/code-yeongyu/oh-my-openagent)
    
    Co-authored-by: Sisyphus <clio-agent@sisyphuslabs.ai>

--- src/components/layout/AppLayout.tsx
+++ src/components/layout/AppLayout.tsx
@@ -147,78 +147,80 @@ class AppLayout extends Component<PropsWithChildren<IProps>, IState> {
       activeModule === 'service-type' && activeServiceTab === 'messages';
 
     const renderMainContent = () => {
-      if (activeModule === 'home') {
-        return <HomeScreen />;
-      }
-      if (activeModule === 'knowledge-base') {
-        return <KnowledgeBaseScreen />;
-      }
-      if (activeModule === 'service-type' && activeServiceTab === 'account') {
-        return <AccountManagementScreen />;
-      }
-      if (activeModule === 'service-type' && activeServiceTab === 'profile') {
-        return <UserProfileScreen />;
-      }
+      const isMessages = isServiceTypeMessagesMode;
       return (
         <>
-          <WorkspaceSwitchingIndicator />
-          {!areRequiredRequestsSuccessful && showRequiredRequestsError && (
-            <InfoBar
-              type="danger"
-              ctaLabel="Try again"
-              ctaLoading={areRequiredRequestsLoading}
-              sticky
-              onClick={retryRequiredRequests}
-            >
-              <Icon icon={mdiFlash} />
-              {intl.formatMessage(messages.requiredRequestsFailed)}
-            </InfoBar>
-          )}
-          {authRequestFailed && (
-            <InfoBar
-              type="danger"
-              ctaLabel="Try again"
-              ctaLoading={areRequiredRequestsLoading}
-              sticky
-              onClick={retryRequiredRequests}
-            >
-              <Icon icon={mdiFlash} />
-              {intl.formatMessage(messages.authRequestFailed)}
-            </InfoBar>
-          )}
-          {automaticUpdates &&
-            showServicesUpdatedInfoBar &&
-            this.state.shouldShowServicesUpdatedInfoBar && (
+          <div className={`flex flex-1 flex-col${isMessages ? '' : ' hidden'}`}>
+            <WorkspaceSwitchingIndicator />
+            {!areRequiredRequestsSuccessful && showRequiredRequestsError && (
               <InfoBar
-                type="primary"
-                ctaLabel={intl.formatMessage(messages.buttonReloadServices)}
-                onClick={() => window.location.reload()}
-                onHide={() => {
-                  this.setState({
-                    shouldShowServicesUpdatedInfoBar: false,
-                  });
-                }}
+                type="danger"
+                ctaLabel="Try again"
+                ctaLoading={areRequiredRequestsLoading}
+                sticky
+                onClick={retryRequiredRequests}
               >
-                <Icon icon={mdiPowerPlug} />
-                {intl.formatMessage(messages.servicesUpdated)}
+                <Icon icon={mdiFlash} />
+                {intl.formatMessage(messages.requiredRequestsFailed)}
               </InfoBar>
             )}
-          {automaticUpdates &&
-            (appUpdateIsDownloaded || (isSnap && isUpdateAvailable)) &&
-            this.state.shouldShowAppUpdateInfoBar && (
-              <AppUpdateInfoBar
-                onInstallUpdate={installAppUpdate}
-                updateVersionParsed={updateVersionParse(updateVersion)}
-                onHide={() => {
-                  this.setState({ shouldShowAppUpdateInfoBar: false });
-                }}
-              />
+            {authRequestFailed && (
+              <InfoBar
+                type="danger"
+                ctaLabel="Try again"
+                ctaLoading={areRequiredRequestsLoading}
+                sticky
+                onClick={retryRequiredRequests}
+              >
+                <Icon icon={mdiFlash} />
+                {intl.formatMessage(messages.authRequestFailed)}
+              </InfoBar>
             )}
-          <BasicAuth />
-          <QuickSwitch />
-          <PublishDebugInfo />
-          {services}
-          <Outlet />
+            {automaticUpdates &&
+              showServicesUpdatedInfoBar &&
+              this.state.shouldShowServicesUpdatedInfoBar && (
+                <InfoBar
+                  type="primary"
+                  ctaLabel={intl.formatMessage(messages.buttonReloadServices)}
+                  onClick={() => window.location.reload()}
+                  onHide={() => {
+                    this.setState({
+                      shouldShowServicesUpdatedInfoBar: false,
+                    });
+                  }}
+                >
+                  <Icon icon={mdiPowerPlug} />
+                  {intl.formatMessage(messages.servicesUpdated)}
+                </InfoBar>
+              )}
+            {automaticUpdates &&
+              (appUpdateIsDownloaded || (isSnap && isUpdateAvailable)) &&
+              this.state.shouldShowAppUpdateInfoBar && (
+                <AppUpdateInfoBar
+                  onInstallUpdate={installAppUpdate}
+                  updateVersionParsed={updateVersionParse(updateVersion)}
+                  onHide={() => {
+                    this.setState({ shouldShowAppUpdateInfoBar: false });
+                  }}
+                />
+              )}
+            <BasicAuth />
+            <QuickSwitch />
+            <PublishDebugInfo />
+            {services}
+            <Outlet />
+          </div>
+
+          {!isMessages && activeModule === 'home' && <HomeScreen />}
+          {!isMessages && activeModule === 'knowledge-base' && (
+            <KnowledgeBaseScreen />
+          )}
+          {!isMessages &&
+            activeModule === 'service-type' &&
+            activeServiceTab === 'account' && <AccountManagementScreen />}
+          {!isMessages &&
+            activeModule === 'service-type' &&
+            activeServiceTab === 'profile' && <UserProfileScreen />}
         </>
       );
     };
